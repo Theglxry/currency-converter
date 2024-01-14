bbd318ac52a2adb8167ad8f31c5b1f4d55e40f3b
Theglxry <102861719+Theglxry@users.noreply.github.com>
2024-01-14 11:24:20 +0100

    update selectCountry

--- src/components/SelectCountry.jsx
+++ src/components/SelectCountry.jsx
@@ -2,24 +2,24 @@ import { Autocomplete, Grid, TextField } from "@mui/material";
 import useAxios from "../hooks/useAxios";
 
 const SelectCountry = () => {
-    //from the hooks
+  //from the hooks
   const [data] = useAxios("https://restcountries.com/v3.1/all"); //console.log(data);
 
-
   //get only the currencies in the api
-  const dataFilter = data.filter((item) => "currencies" in item); //console.log(dataFilter);
-
-
-  //1- get country & it flag 
-  //2- get the country currency key (EUR,NG,AUD, etc) using Object.keys
-  const dataCountries = data.map((item) => {
-    return `${item.flag} ${object.key.cu }`;
-  }); //console.log(dataCountries);
+  const dataFilter = data.filter((item) => "currencies" in item);
+  console.log(dataFilter);
 
 
 
-
-
+  //1- get country & it flag
+  //2- get the country currency key(EUR,NG,AUD, etc) using Object.keys.  which is the first obj under the currencies[0]
+  //3- get the country name (name.common {on the api log})
+  const dataCountries = dataFilter.map((item) => {
+    return `${item.flag} ${Object.keys(item.currencies)[0]} - ${
+      item.name.common
+    }`;
+  });
+  console.log(dataCountries);
 
   return (
     //xs = breakpoint (12)
