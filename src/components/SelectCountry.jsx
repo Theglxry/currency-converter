989c047373b41fe69b0c2f37b5cd3bd7da482fc0
Theglxry <102861719+Theglxry@users.noreply.github.com>
2024-01-14 11:07:54 +0100

    update

--- src/components/SelectCountry.jsx
+++ src/components/SelectCountry.jsx
@@ -1,9 +1,26 @@
 import { Autocomplete, Grid, TextField } from "@mui/material";
-
-
-
+import useAxios from "../hooks/useAxios";
 
 const SelectCountry = () => {
+    //from the hooks
+  const [data] = useAxios("https://restcountries.com/v3.1/all"); //console.log(data);
+
+
+  //get only the currencies in the api
+  const dataFilter = data.filter((item) => "currencies" in item); //console.log(dataFilter);
+
+
+  //1- get country & it flag 
+  //2- get the country currency key (EUR,NG,AUD, etc) using Object.keys
+  const dataCountries = data.map((item) => {
+    return `${item.flag} ${object.key.cu }`;
+  }); //console.log(dataCountries);
+
+
+
+
+
+
   return (
     //xs = breakpoint (12)
     <Grid item xs={12} md={3}>
--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,20 +1,29 @@
-import { useState } from "react"
+import axios from "axios";
+import { useEffect, useState } from "react";
 
+const useAxios = (url) => {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
+  // fetch data
+//todo->  create a time out later
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const res = await axios(url);
+        setData(res.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]); //the useEffect runs everytime the url is clicked.
 
+  return [data, error, loading];
+};
 
-
-
-
-
-const useAxios = () => {
- const [data, setData] = useState([]);
- const [error, setErro] = useState(null);
- const [loading, setLoading] = useState(false);
-
-// fetch data
-
-  return  [data,error,loading]
-}
-
-export default useAxios
+export default useAxios;
